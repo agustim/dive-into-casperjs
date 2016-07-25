79cdd75ee484e945940ec753553c95a3cbd38dba
Agustí Moll <agusti.moll@guifi.net>
2016-07-25 19:51:54 +0200

    Test fiberfy.io

---
+++ test-fiberfy/test/login-test.js
@@ -0,0 +1,49 @@
+casper.options.viewportSize = {
+    width: 1920,
+    height: 987
+};
+casper.on('page.error', function(msg, trace) {
+    this.echo('Error: ' + msg, 'ERROR');
+    for (var i = 0; i < trace.length; i++) {
+        var step = trace[i];
+        this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
+    }
+});
+casper.test.begin('Fiberfy login test', 5, function suite(test) {
+    casper.start('http://localhost:3000', function() {
+      test.assertTitle("", "fiberfy not homepage title, yet!");
+    });
+
+    casper.then(function(){
+      test.assertExists("form[name=form-login] input[name='username']",
+                        "username field exist.");
+      test.assertExists("form[name=form-login] input[name='password']",
+                        "password field exist.");
+
+      this.fill("form[name=form-login]", {
+          'username': 'demo',
+          'password': 'NO_demo'
+      }, true);
+    });
+    casper.then(function(){
+      test.assertSelectorHasText('#message', 'Please enter right password!');
+    });
+
+    casper.then(function(){
+      this.fill("form[name=form-login]", {
+          'username': 'demo',
+          'password': 'demo'
+      }, true);
+    });
+
+    casper.then(function(){
+      this.wait(500, function() {
+        test.assertUrlMatch(/map/, "Login correct!");
+      })
+
+    });
+    casper.run(function() {
+        test.done();
+    });
+
+});
