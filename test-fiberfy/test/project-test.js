62a116a8be608f180cf0300e5635463df093f79b
Agustí Moll <agusti.moll@guifi.net>
2016-09-02 18:33:14 +0200

    advanced in project test

--- test-fiberfy/test/project-test.js
+++ test-fiberfy/test/project-test.js
@@ -9,11 +9,14 @@ casper.on('page.error', function(msg, trace) {
         this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
     }
 });
-casper.test.begin('Fiberfy project test', 2, function suite(test) {
+
+casper.test.begin('Fiberfy project test', 3, function suite(test) {
+    //var projectName = "demo"+Math.floor(Math.random()*1000);
+    var projectName = "demo1010";
+
     casper.start('http://localhost:3000/');
 
     casper.then(function(){
-      test.assertTitle("", "fiberfy not homepage title, yet!");
       this.waitForSelector("form[name=form-login]", function() {
         this.fillSelectors("form[name=form-login]", {
             'input[name="username"]': 'demo',
@@ -23,7 +26,40 @@ casper.test.begin('Fiberfy project test', 2, function suite(test) {
       this.waitForUrl(/map$/, function() {
         test.pass("Login")
       });
+      this.waitForSelector("a#projects_manager");
     });
+
+    casper.then(function(){
+      this.mouse.click("a#projects_manager");
+      test.assertVisible("#form-project-group","View project page.")
+    });
+
+    casper.then(function(){
+      this.fillSelectors("div#form-project-group", {
+        'input[name=project-name]': projectName
+      }, false);
+
+      var projectItemsBefore = this.evaluate(function(){
+        return(__utils__.findAll('.project-item'))
+      });
+      this.log(projectItemsBefore,'warning');
+
+      this.mouse.click('button#project-add');
+
+      casper.waitFor(function check() {
+          return this.evaluate(function() {
+              return document.querySelectorAll('.project-item').length > projectItemsBefore;
+          });
+        }, function then(){
+          test.pass("Create project.");
+        });
+      });
+    })
+
+    casper.then(function(){
+
+    })
+
 
     casper.run(function() {
         test.done();
